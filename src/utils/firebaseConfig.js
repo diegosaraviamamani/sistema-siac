b53e66773d5f22dbd41d6fe576525c962860fec2
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-03-18 21:44:23 -0400

    configuracion de firebase

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getDatabase } from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcRbZ-OVffdgte47Sw3IAPUGrfjluDj0s",
+  authDomain: "sistema-siac.firebaseapp.com",
+  projectId: "sistema-siac",
+  storageBucket: "sistema-siac.appspot.com",
+  messagingSenderId: "116372310561",
+  appId: "1:116372310561:web:17e344cda12faac0857160"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(firebaseApp)
+export const db = getDatabase(firebaseApp)
