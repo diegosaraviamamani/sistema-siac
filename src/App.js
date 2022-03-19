52f0ffe4debcf1c6a2c61f5f675b413f116d5f32
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-03-18 23:23:04 -0400

    vista de consultas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Clients from './pages/Clients';
 import Login from './pages/Login'
+import Query from './pages/Query';
 import Results from './pages/Results';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<div>Inicio</div>} />
-      <Route path='/about' element={<div>About</div>} />
+      <Route path='/' element={<Query />} />
       <Route path='/login' element={<Login />} />
       <Route path='/clientes' element={<Clients />} />
       <Route path='/resultados' element={<Results />} />
---
+++ src/pages/Query.js
@@ -0,0 +1,21 @@
+import { Container, Stack, Typography } from "@mui/material"
+
+const containerStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'strech',
+  minHeight: '100vh'
+}
+
+function Query() {
+  return (
+    <Container maxWidth="lg" sx={containerStyles}>
+      <Stack spacing={2}>
+        <Typography variant="h5" align="center">
+          CONSULTA
+        </Typography>
+      </Stack>
+    </Container>
+  )
+}
+export default Query
