a941cadb2115a8144c9038f145eea4dad5d76386
MarceEspinoza <march_es_t@outlook.es>
2022-03-19 00:22:03 -0400

    Vista de consultas

--- src/pages/Query.js
+++ src/pages/Query.js
@@ -1,19 +1,41 @@
-import { Container, Stack, Typography } from "@mui/material"
+import { Button, Container, Stack, TextField, Typography } from "@mui/material"
+import { useState } from "react"
 
 const containerStyles = {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'strech',
-  minHeight: '100vh'
+  justifyContent: 'center',
+  minHeight: '100vh',
 }
 
 function Query() {
+  const [queryData, setQueryData] = useState({ order: "", ci: "" })
   return (
-    <Container maxWidth="lg" sx={containerStyles}>
+    <Container maxWidth="xs" sx={containerStyles}>
       <Stack spacing={2}>
         <Typography variant="h5" align="center">
           CONSULTA
         </Typography>
+        <TextField
+          value={queryData.order}
+          onChange={(e) => setQueryData({ ...queryData, order: e.target.value })}
+          label="Nro de Orden"
+          variant="outlined"
+        />
+        <TextField
+          value={queryData.ci}
+          onChange={(e) => setQueryData({ ...queryData, ci: e.target.value })}
+          label="Carnet de Identidad"
+          variant="outlined"
+        />
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => console.log(queryData)}
+        >
+          Aceptar
+        </Button>
       </Stack>
     </Container>
   )
