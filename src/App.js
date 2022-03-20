b83005cdd14447211a18b16f3ed933e7be1e21ae
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-03-19 20:19:33 -0400

    redireccion en base a usuario logueado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { useAuthContext } from './contexts/AuthContext';
 import Clients from './pages/Clients';
 import Login from './pages/Login'
 import Query from './pages/Query';
 import Results from './pages/Results';
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <Routes>
       <Route path='/' element={<Query />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/clientes' element={<Clients />} />
-      <Route path='/resultados' element={<Results />} />
+      <Route path='/login' element={!user ? <Login /> : <Navigate to='/clientes' />} />
+      <Route path='/clientes' element={user ? <Clients /> : <Navigate to='/login' />} />
+      <Route path='/resultados' element={user ? <Results /> : <Navigate to='/login' />} />
     </Routes>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import { Button, Container, Stack, TextField, Typography } from "@mui/material"
-import { useEffect, useState } from "react"
-import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth"
+import { useState } from "react"
+import { signInWithEmailAndPassword, signOut } from "firebase/auth"
 import { firebaseAuth } from "../utils/firebaseConfig"
 
 const loginContainerStyles = {
@@ -17,13 +17,6 @@ function Login() {
   async function login() {
     await signInWithEmailAndPassword(firebaseAuth, credentials.email, credentials.password)
   }
-
-  useEffect(() => {
-    const salir = onAuthStateChanged(firebaseAuth, (user) => {
-      console.log(user)
-    })
-    return salir
-  }, [])
 
   return (
     <Container maxWidth="xs" sx={loginContainerStyles}>
