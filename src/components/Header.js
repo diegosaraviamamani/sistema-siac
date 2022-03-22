60c81f2bf4bc8e5bd5b7d81d94730f50bb493532
Noelia Iris <noelia.2843@gmail>
2022-03-21 23:05:04 -0400

    Formulario agregado al header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,21 @@ import LogoutIcon from "@mui/icons-material/Logout"
 import { AppBar, Container, IconButton, Toolbar, Typography } from "@mui/material"
 import { signOut } from "firebase/auth"
 import { firebaseAuth } from "../utils/firebaseConfig"
+import NewClientForm from "./NewClientForm"
 
 function Header() {
-
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
-
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          SIAC
-        </Typography>
-        <IconButton color="inherit"  onClick={() => signOut(firebaseAuth)}>
-          <LogoutIcon />
-        </IconButton>
-      </Toolbar>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            SIAC
+          </Typography>
+          <NewClientForm />
+          <IconButton color="inherit" onClick={() => signOut(firebaseAuth)}>
+            <LogoutIcon />
+          </IconButton>
+        </Toolbar>
       </Container>
     </AppBar>
   )
