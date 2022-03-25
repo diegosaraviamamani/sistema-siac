fd81ef4cc5d6e66c57d7516159bded9e8d4d7635
Diego Saravia Mamani <63614444+diegosaraviamamani@users.noreply.github.com>
2022-03-24 23:03:17 -0400

    boton de volver agregado

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,29 +1,51 @@
-import { useEffect, useState } from "react";
-import { collection, getDocs, query } from "firebase/firestore";
-import { useParams } from "react-router-dom";
-import { Container, IconButton, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
-import Header from "../components/Header"
-import { db } from "../utils/firebaseConfig";
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import DeleteIcon from "@mui/icons-material/Delete";
-import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { useEffect, useState } from 'react'
+import { collection, getDocs, query } from 'firebase/firestore'
+import { useNavigate, useParams } from 'react-router-dom'
+import {
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material'
+import Header from '../components/Header'
+import { db } from '../utils/firebaseConfig'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import DeleteIcon from '@mui/icons-material/Delete'
+import UploadFileIcon from '@mui/icons-material/UploadFile'
 
 const containerStyles = {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'strech',
-  minHeight: '100vh'
+  height: '100%',
 }
 async function getData(setresults, ci) {
-  const result = await getDocs(query(collection(db, 'clients', ci, 'testResults')));
-  setresults(result ? result.docs.map((c) => ({ order: c.id, ...c.data() })) : [])
+  const result = await getDocs(
+    query(collection(db, 'clients', ci, 'testResults'))
+  )
+  setresults(
+    result ? result.docs.map((c) => ({ order: c.id, ...c.data() })) : []
+  )
 }
 function Results() {
   const [results, setresults] = useState([])
   const { ci } = useParams()
+  const navigate = useNavigate()
   function todate(seconds) {
     const date = new Date(seconds * 1000)
     return new Intl.DateTimeFormat('es-ES').format(date)
+  }
+  //handle return with navigate
+  const handleReturn = () => {
+    navigate(-1)
   }
   useEffect(() => {
     getData(setresults, ci)
@@ -33,7 +55,10 @@ function Results() {
       <Header />
       <Container maxWidth="lg" sx={containerStyles}>
         <Stack spacing={2}>
-          <Typography variant="h5" align="center">
+          <Button onClick={handleReturn} sx={{ alignSelf: 'end' }}>
+            Volver
+          </Button>
+          <Typography variant="h5" align="center" sx={{ letterSpacing: 5 }}>
             REPORTE DE RESULTADOS
           </Typography>
           <TableContainer component={Paper}>
@@ -56,16 +81,18 @@ function Results() {
                     <TableCell component="th" scope="row">
                       {row.order}
                     </TableCell>
-                    <TableCell align="right">{todate(row.createdAt.seconds)}</TableCell>
+                    <TableCell align="right">
+                      {todate(row.createdAt.seconds)}
+                    </TableCell>
                     <TableCell align="right">{row.type}</TableCell>
-                    <TableCell align="right">{row.uploadetAt && todate(row.uploadedAt.seconds)}</TableCell>
+                    <TableCell align="right">
+                      {row.uploadetAt && todate(row.uploadedAt.seconds)}
+                    </TableCell>
                     <TableCell align="right">
                       <IconButton color="error">
                         <DeleteIcon />
                       </IconButton>
-                      <IconButton
-                        color="success"
-                      >
+                      <IconButton color="success">
                         <VisibilityIcon />
                       </IconButton>
                       <IconButton color="primary">
@@ -77,12 +104,9 @@ function Results() {
               </TableBody>
             </Table>
           </TableContainer>
-
         </Stack>
-
       </Container>
     </Stack>
-
   )
 }
 export default Results
